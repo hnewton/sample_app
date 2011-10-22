e059e28a7ec1f3f65c1872b320b359f0db2be627
Hal Newton <hal.newton@gmail.com>
2011-10-22 13:57:55 -0700

    add stub for User show

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,9 +15,8 @@ class User < ActiveRecord::Base
   validates :name,  :presence => true, 
                     :length => { :maximum => 50}
   validates :email, :presence => true, 
-                    :format   => { :with => email_regex }
-                    :uniqueness => {:case_snesitive => false} 
-  
+                    :format   => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false} 
 end
 
 
